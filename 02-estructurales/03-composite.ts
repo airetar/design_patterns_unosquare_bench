ea21bf07d772b92b66e482af48007e6da1b1a29d
Andrés Ireta <andres.ireta@unosqueare.com>
2025-03-19 09:29:39 -0600

    progress designs

--- 02-estructurales/03-composite.ts
+++ 02-estructurales/03-composite.ts
@@ -13,3 +13,68 @@
  * https://refactoring.guru/es/design-patterns/composite
  *
  */
+
+interface FileSystemComponent {
+    showDetails(indent?: string): void;
+}
+
+class File implements FileSystemComponent {
+    private name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    showDetails(indent?: string): void {
+      console.log(`${indent}- Archivo: ${this.name}`);
+    }
+}
+
+class Folder implements FileSystemComponent {
+    private name: string;
+    private contents: FileSystemComponent[] = [];
+
+    constructor( name: string ) {
+        this.name = name;
+    }
+
+    add( component: FileSystemComponent ) {
+        this.contents.push(component);
+    }
+
+    showDetails(indent?: string): void {
+        console.log(`${ indent }+ Carpeta: ${ this.name }`);
+        this.contents.forEach(component => component.showDetails(indent + ' '))
+    }
+}
+
+function main() {
+    const file1 = new File('archivo1.txt');
+    const file2 = new File('archivo2.txt');
+    const file3 = new File('archivo3.txt');
+    const file4 = new File('archivo4.txt');
+    const file5 = new File('archivo5.txt');
+
+    const folder1 = new Folder('carpeta 1');
+    folder1.add( file1 );
+    folder1.add( file2 );
+
+    const folder2 = new Folder('carpeta 2');
+    folder2.add( file3 );
+
+    const folder3 = new Folder('carpeta 3');
+    folder3.add( file5 );
+    folder3.add( file4 );
+
+    const rootFolder = new Folder('root');
+
+    folder2.add(folder3);
+    folder1.add(folder2);
+    rootFolder.add(folder1);
+
+    rootFolder.showDetails('   ');
+
+
+}
+
+main();
